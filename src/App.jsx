b2d0b055ddba2578f4bcc8bdafb1c9aa407e563d
jkidwell40 <jkidwell40@hotmail.com>
2025-04-17 20:20:11 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -173,10 +173,10 @@ export default function App() {
   ))}
 </div>
 
-      <button
+      <button>
   onClick={() => setModalStep('add')}
   style={{ position: 'fixed', top: 10, left: 10, zIndex: 1000 }}
->
+
   Add Belief
 </button>
 
